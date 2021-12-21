5890524d1850aa6b10342965121ed75cd9b6ec90
PabloGomeez <90782711+PabloGomeez@users.noreply.github.com>
2021-12-21 06:17:25 -0500

    Update config.js

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export const dbConnection = async () =>{
     try {
         await mongoose.connect("mongodb+srv://admin:root@cluster0.ayz2x.mongodb.net/tiendaEsotericaProyecto2?retryWrites=true&w=majority");
-        //await mongoose.connect("mongodb+srv://misiontic2022_1:SoluWeb2022@cluster0.rjlj0.mongodb.net/gestorProyectos?retryWrites=true&w=majority");
         console.log("DB conectada")
         
     } catch (error) {
